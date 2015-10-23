35f9691543206744acc9acea9340f95779f5f469
Amine MBARKI <a.mbarki@dm73.net>
2015-10-23 17:58:06 +0100

    chrome extension

--- src/console/background.js
+++ src/console/background.js
@@ -1,10 +1,41 @@
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.method == "getDom"){
-            sendResponse({data: document.all[0].outerHTML}); //same as innerText
-        }
-        if(request.method == "getDomain"){
-            sendResponse({data: document.domain}); //same as innerText
-        }
+//chrome.runtime.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//        if(request.method == "getDom"){
+//            sendResponse({data: document.all[0].outerHTML}); //same as innerText
+//        }
+//        if(request.method == "getDomain"){
+//            sendResponse({data: document.domain}); //same as innerText
+//        }
+//    });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // No tabs or host permissions needed!
+    console.log('Turning ' + tab.url + ' red!');
+    chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
     });
+});
+
+function updateSelected(tabId) {
+    console.log('Turning ' + tabId + ' red!');
+}
+function updateAddress(tabId) {
+    console.log('Turning ' + tabId + ' red!');
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+    if (change.status == "complete") {
+        updateAddress(tabId);
+    }
+});
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
+    //selectedId = tabId;
+    updateSelected(tabId);
+});
+
+// Ensure the current selected tab is set up.
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    updateAddress(tabs[0].id);
+});
