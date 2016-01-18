9a223da12b8e213677d08c9db4ee44f76ff16235
Amine Mbarki <aminembarki@gmail.com>
2016-01-18 00:35:04 +0000

    change  project name

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-var context = {
-    "name":"Ritesh Kumar",
-    "occupation" : "Developer"
-}
 
-var templateScript = Handlebars.templates.error(context);
 
-document.write(templateScript);
+$(function(){
+
+    chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.executeScript(tab.id, { file: "src/console/content.script.js", runAt: "document_end" }, function (response) { });
+    });
+
+    var context = {
+        "name":"Ritesh Kumar",
+        "occupation" : "Developer"
+    }
+
+    var template = Handlebars.templates.error(context);
+    template = template + Handlebars.templates.navbar(context);
+
+
+
+    $('#content-wrapper').html(template);
+
+});
